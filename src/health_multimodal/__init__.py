f6732a8535c9045de3e04568b7dc73c80765f005
Taehun <97962327+Castella99@users.noreply.github.com>
2024-03-04 01:25:21 +0900

    Delete src/health_multimodal directory

--- src/health_multimodal/__init__.py
+++
@@ -1,6 +0,0 @@
-#  -------------------------------------------------------------------------------------------
-#  Copyright (c) Microsoft Corporation. All rights reserved.
-#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-#  -------------------------------------------------------------------------------------------
-
-__version__ = "0.2.1"
